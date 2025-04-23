f5e7e6cdc6bead2fee08d6ebbdf36375cfd985a3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-23 13:07:14 +0530

    Authentication with JWT COOKIES

---
+++ Auth/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const DATABASE_URI = process.env.DATABASE_URI
+const connectDB = async () => {
+    try{
+        await mongoose.connect(DATABASE_URI);
+        console.log("Database connection successfull")
+    }   
+    catch(error) {
+        console.log(error.message);
+        console.log("Failed to Connect Database")
+        process.exit(0)
+    }
+}
+module.exports = connectDB
---
+++ Auth/main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const app_routes = require('./routes/app_routes')
+const connectDB = require('./config/database')
+const PORT = process.env.PORT
+
+
+app.use(express.json())
+
+app.use('/api/v1' , app_routes);
+
+
+app.listen(PORT , () => {
+    console.log(`App is running on PORT ${PORT}`)
+})
+
+connectDB();
