78790ffad45142c92209e775a1c4a7a62ee8a0f5
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-23 14:22:30 +0530

    signUp handler

--- Auth/controllers/signUp.js
+++ Auth/controllers/signUp.js
@@ -1,12 +1,26 @@
 const User = require('../model/UserSchema')
-
+const bcrypt = require('bcrypt')
 const signUp = async (req , res) => {
     try{
         const { username , email , password , role} = req.body;
+
+
+        // check if user already exists
+        const userExist = await User.findOne({ email })
+        if(userExist) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+        // Encrypt password
+        const saltRound = 10;
+        const hash_password = await bcrypt.hash(password , saltRound);
+
         const createUser = await User.create({
             username,
             email,
-            password,
+            password: hash_password,
             role
         });
 
