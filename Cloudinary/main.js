e7e4961be3427b7ff08fae63e9420c6c3e8eac99
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-25 23:15:06 +0530

    full setup of cloudinary media upload project #Backend

--- Cloudinary/main.js
+++ Cloudinary/main.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload')
 require('dotenv').config();
 const app_routes = require('./routes/app_routes')
 const connectDB = require('./config/database')
+const cloudinary = require('./config/cloudinary')
 const PORT = process.env.PORT
 
 
 app.use(express.json())
+app.use(fileUpload())
 app.use('/api/v1' , app_routes);
 
 app.get('/', (req , res) => {
@@ -18,3 +21,4 @@ app.listen(PORT , () => {
 })
 
 connectDB()
+cloudinary.cloudinaryConnect()
