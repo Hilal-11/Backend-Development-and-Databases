09b92db2ee49cffd64533a9d33a0623386899985
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-26 01:02:11 +0530

    done local file upload

--- Cloudinary/controllers/localFileUpload.js
+++ Cloudinary/controllers/localFileUpload.js
@@ -5,14 +5,15 @@ const localFileUpload = async ( req , res) => {
         const file = req.files.file;
         console.log(file);
 
-        let path = __dirname + "/files/" + Date.now()
+        let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`
         console.log(path)
         file.mv(path , (err) => {
             console.log(err)
         });
 
         res.json({
-            success: true
+            success: true,
+            message: "Local file is upload successfully"
         })
     }catch(error) {
         console.log(error.message);
