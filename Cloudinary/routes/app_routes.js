7eb68072f8414949d19ffc2d2b6608a02a9a017d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-25 22:33:00 +0530

    done

--- Cloudinary/routes/app_routes.js
+++ Cloudinary/routes/app_routes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const imageUpload = require('../controllers/imageUpload')
+const videoUpload = require('../controllers/videoUpload')
+const imageReducerUpload = require('../controllers/imageReducerUpload')
+const localFileUpload = require('../controllers/localFileUpload')
 
 
-router.post('/', );
-router.post('/', );
-router.post('/', );
-router.post('/', );
+router.post('/imageUpload', imageUpload);
+router.post('/videoUpload', videoUpload);
+router.post('/imageReducerUpload', imageReducerUpload);
+router.post('/localFileUpload', localFileUpload);
 
 
 module.exports = router
