a3518d60ff309b013d83facd89288544199aa400
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-04 01:19:36 +0530

    nodemailer frustration OTP mail pusher

--- Cloudinary/models/File.js
+++ Cloudinary/models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const nodemailer = require('nodemailer');
 
 const FileSchema = new mongoose.Schema({
     name: {
@@ -16,4 +17,26 @@ const FileSchema = new mongoose.Schema({
         required: true
     },
 })
+
+
+FileSchema.post('save' , async function(doc)  {
+    try{
+        console.log(doc)
+
+        // Transpoter
+
+        let transpoter = nodemailer.transpoter({
+            host: process.env.MAIL_HOST,
+            port: '',
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            }
+        })
+    }catch(error){
+        console.log(error.message)
+    }
+})
+
+
 module.exports = mongoose.model('File' , FileSchema)
