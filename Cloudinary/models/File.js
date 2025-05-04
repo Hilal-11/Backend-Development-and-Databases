3d18f21a36b13b142ce4d66e182b55e9b600b84f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-04 14:10:46 +0530

    nodemaiker

--- Cloudinary/models/File.js
+++ Cloudinary/models/File.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const nodemailer = require('nodemailer');
-
+require('dotenv').config()
 const FileSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -19,20 +19,29 @@ const FileSchema = new mongoose.Schema({
 })
 
 
-FileSchema.post('save' , async function(doc)  {
+FileSchema.post('save' , async function(docs)  {
     try{
-        console.log(doc)
-
         // Transpoter
-
         let transpoter = nodemailer.transpoter({
-            host: process.env.MAIL_HOST,
-            port: '',
+            host: 'smtp-relay.brevo.com',
+            port: 587,
             auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            },
+            secure: false,
+            tls: {
+                rejectUnauthorized: false, // <-- allow self-signed certificates
             }
         })
+        // email send
+        let info = await transpoter.sendMail({
+            from: 'Webmastery.pro',
+            to: docs.email,
+            subject: 'Hii there i am Hilal from webMastery.pro',
+            text: 'Hello everyone',
+            html: '<b>Webmastery.pro</b>'
+        });
     }catch(error){
         console.log(error.message)
     }
