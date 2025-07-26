0d9131e2cb9a4da5441a47cfff3c7668b1528c16
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-26 13:54:42 +0530

    done

--- Cloudinary/controllers/localFileUpload.js
+++ Cloudinary/controllers/localFileUpload.js
@@ -6,7 +6,7 @@ const localFileUpload = async ( req , res) => {
         const file = req.files.file;
         console.log(file);
 
-        // EXTRACT THE FILE PATG AND FILE EXTENSION || 
+        // EXTRACT THE FILE PATH AND FILE EXTENSION || 
         let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`
         console.log(path)
 
