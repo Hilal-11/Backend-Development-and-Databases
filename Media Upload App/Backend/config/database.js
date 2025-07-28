2437ee64afbe209f63c9eab852df6e5a199c33b8
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-28 15:14:22 +0530

    done

---
+++ Media Upload App/Backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+const DATABASE_URL = process.env.DATABASE_URL;
+
+
+const connectDB =  () => {
+    mongoose.connect(DATABASE_URL)
+    .then(() => {
+        console.log("Database Connection Successfully")
+    }).catch((error) => {
+        console.log(error.message)
+        console.log("Database Connection Failed")
+        process.exit(1)
+    })
+}
+
+module.exports = connectDB
