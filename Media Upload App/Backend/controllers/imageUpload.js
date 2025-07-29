3f5eedb086d28af6bd3d177701d1918e0808deac
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-29 19:36:21 +0530

    done 3

--- Media Upload App/Backend/controllers/imageUpload.js
+++ Media Upload App/Backend/controllers/imageUpload.js
@@ -1,15 +1,54 @@
 const Model = require('../models/MediaModel.model')
+const cloudinary = require('cloudinary')
 const imageUpload = async (req , res) => {
+
+    function isSupportedType(type , supportedTypes) {
+        return supportedTypes.includes(type)
+    }
+    async function uplodaFileToCloudinary(file , folder) {
+        return await cloudinary.uploader.upload(file.tempFilePath , {
+            folder: folder,
+            resource_type: "auto"
+        })
+    } 
+
     try{   
-        const { name , tag , email , fileUrl} = req.body;
-        const file = req.files.file
+        const { name , tag , email , fileUrl } = req.body;
+        const file = req.files.imageFile
+        console.log(file)
         
-        const support = ['jpg', 'png', 'jpeg', 'webg']
-        const exension = file.split('.')[1]
+        // VALIDATION
+        const supportedTypes = ['jpg', 'png', 'jpeg', 'webg']
+        const fileType = file.name.split('.')[1].toLowerCase()
+
+        const isSupported = isSupportedType(fileType , supportedTypes)
+        if(!isSupported) {
+            return res.status(401).json({
+                success: false,
+                message: "File type not supported"
+            })
+        }
+        // Upload on Cloudinary
+
+        const response = await uplodaFileToCloudinary(file , "Solitude")
+        console.log(response)
+        // db entry
+        const fileData = await Model.create({
+            name,
+            tag,
+            email,
+            fileUrl: response.secure_url
+        })
+        
+        res.status(200).json({
+            success: true,
+            message: "successfully upload on cloudinary and save entry on DB"
+        })
+
 
     }catch(error) {
         console.log(error.message)
-        res.status().json({
+        res.status(400).json({
             success: false,
             error: error.message,
             message: "failed to upload image on clouddinary",
