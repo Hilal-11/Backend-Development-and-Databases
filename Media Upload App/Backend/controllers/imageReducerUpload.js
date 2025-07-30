790ee68a30f17444193b227575352d0a8677181d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-30 13:59:46 +0530

    image reducer uoload

---
+++ Media Upload App/Backend/controllers/imageReducerUpload.js
@@ -0,0 +1,58 @@
+const Model = require('../models/MediaModel.model')
+const cloudinary = require('cloudinary');
+
+const imageReducerUpload = async (req , res) => {
+    function isSupportedType(type , supportedTypes) {
+        return supportedTypes.includes(type)
+    }
+    async function imageReducerUploadToCloudinary(file , folder) {
+        return await cloudinary.uploader.upload(file.tempFilePath , {
+            resource_type: "image",
+            transformation: [
+                { quality: "auto" , fetch_format: "auto" }
+            ]  
+        })
+    }
+    try {
+        const  {name, tag , email , imageUrl } = req.body;
+        const file = req.files.imageFile;
+
+        // validation
+        const supportedTypes = ['jpg' , 'png' , 'jpeg', 'webp'];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        const isSupported = isSupportedType(fileType , supportedTypes)
+
+        if(!isSupported) {
+            return res.status(401).json({
+                success: false,
+                message: "File type not supported"
+            })
+        }
+
+
+        // upload on cloudinary
+        const response = await imageReducerUploadToCloudinary(file , "Solitude")
+
+        // entry on database
+        const fileData = await Model.create({
+            name,
+            tag,
+            email,
+            fileUrl: response.secure_url
+        })
+
+        res.status(201).json({
+            success: true,
+            message: "Successfully upload on cloudinary and entry on database"
+        })
+
+    }catch(error) {
+        console.log(error.message);
+        res.status(401).json({
+            success: false,
+            error: error.message,
+            message: "Failed to upload reduce image file on cloudinary"
+        })
+    }
+}
+module.exports = imageReducerUpload
