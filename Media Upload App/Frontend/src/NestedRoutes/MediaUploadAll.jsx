fb75ea6f26130056f69130c56e55b5055414bfc4
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-31 22:20:40 +0530

    frontend done

--- Media Upload App/Frontend/src/NestedRoutes/MediaUploadAll.jsx
+++ Media Upload App/Frontend/src/NestedRoutes/MediaUploadAll.jsx
@@ -2,13 +2,28 @@ import React from 'react'
 
 function MediaUploadAll() {
   return (
-    <div className='w-full h-[600px] flex flex-wrap justify-evenly gap-10'>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+    <div className='grid grid-cols-2 gap-3 w-full h-[600px] lg:flex lg:flex-wrap justify-evenly lg:gap-6'>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
     </div>
   )
 }
