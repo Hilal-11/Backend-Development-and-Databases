dd8778baa8a84ddbfd0bcc18e11aee6285eb2634
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-01 00:37:07 +0530

    frontend and backend connection done

--- Media Upload App/Frontend/src/NestedRoutes/ImageUploadCloudinary.jsx
+++ Media Upload App/Frontend/src/NestedRoutes/ImageUploadCloudinary.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import { AppContext } from '../../Context/CreateContext'
 function ImageUploadCloudinary() {
+
+  const { cloudinaryData } = useContext(AppContext);
+
+
   return (
     <div className='grid grid-cols-2 gap-3 w-full lg:flex lg:flex-wrap justify-evenly lg:gap-6'>
-      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
-        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
-      </div>
-      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
-        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
-      </div>
-      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
-        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
-      </div>
-      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
-        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
-      </div>
+      {
+        cloudinaryData.map((item) => (
+          <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+            <img className="w-full h-full rounded-lg" src={item.fileUrl} alt="Error" />
+          </div>
+        ))
+      }
     </div>
   )
 }
