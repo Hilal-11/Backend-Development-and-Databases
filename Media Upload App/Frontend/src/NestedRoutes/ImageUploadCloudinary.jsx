fc90e440349f6602181d4ebc47c7f25b027d06e2
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-31 22:21:54 +0530

    frontend done

--- Media Upload App/Frontend/src/NestedRoutes/ImageUploadCloudinary.jsx
+++ Media Upload App/Frontend/src/NestedRoutes/ImageUploadCloudinary.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 function ImageUploadCloudinary() {
   return (
-    <div className='w-full h-[600px] flex flex-wrap justify-evenly gap-10'>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+    <div className='grid grid-cols-2 gap-3 w-full lg:flex lg:flex-wrap justify-evenly lg:gap-6'>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'>
+        <img className="w-full h-full rounded-lg" src="https://res.cloudinary.com/dou5rypdf/image/upload/v1753961904/kmltz7ypabdojpdwk4ud.jpg" alt="" />
+      </div>
     </div>
   )
 }
