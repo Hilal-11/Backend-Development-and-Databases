534a563fa5d404d52c93d1610174d7706a7ea742
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-31 22:21:10 +0530

    frontend done

--- Media Upload App/Frontend/src/NestedRoutes/DocsUploadCloudinary.jsx
+++ Media Upload App/Frontend/src/NestedRoutes/DocsUploadCloudinary.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 function DocsUploadCloudinary() {
   return (
-    <div className='w-full h-[600px] flex flex-wrap justify-evenly gap-10'>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
-      <div className='w-[350px] h-[400px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+    <div className='grid grid-cols-2 gap-3 w-full h-[600px] lg:flex lg:flex-wrap justify-evenly lg:gap-6'>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
+      <div className='lg:w-[360px] lg:h-[300px] rounded-lg bg-white shadow-sm shadow-slate-800 cursor-pointer'></div>
     </div>
   )
 }
