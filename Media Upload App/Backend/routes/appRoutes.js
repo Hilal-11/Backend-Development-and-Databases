175d81e3987156e8eb323aa0827a50b31cb16683
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-01 15:44:52 +0530

    done

--- Media Upload App/Backend/routes/appRoutes.js
+++ Media Upload App/Backend/routes/appRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router();
 
+const getMediaUpoadCloudinaryData = require('../controllers/getMediaUploadCloudinaryData')
 const imageUpload = require('../controllers/imageUpload')
 const videoUplaod = require('../controllers/videoUpload')
 const imageReducerUpload = require('../controllers/imageReducerUpload')
 const localFileUpload = require('../controllers/localFileUpload')
 
+router.get('/getMediaUploadCloudinaryData', getMediaUpoadCloudinaryData);
 router.post('/imageUpload', imageUpload);
 router.post('/videoUpload', videoUplaod);
 router.post('/imageReducerUplead', imageReducerUpload);
