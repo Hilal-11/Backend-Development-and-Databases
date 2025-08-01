687ff77cac8f73705deb9ea69eb19812d342923f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-01 15:44:08 +0530

    backend

--- Media Upload App/Backend/main.js
+++ Media Upload App/Backend/main.js
@@ -8,7 +8,7 @@ const fileUpload = require('express-fileupload')
 const PORT = process.env.PORT
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions))
